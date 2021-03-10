00bd64860de5bfb228f150bf164f09eeb889b19c
Robert Carr <racarr@Roberts-MacBook-Pro.local>
2021-03-09 21:17:12 -0800

    Advanced Mitch Detection

--- mmd/spacy_mmd/spacy_mmd.py
+++ mmd/spacy_mmd/spacy_mmd.py
@@ -1,7 +1,10 @@
 import spacy
 class SpacyModel:
     def __init__(self):
-        self.nlp = spacy.load("en_core_web_trf")
+        self.nlp = spacy.load("en_core_web_lg")
     def get_entities(self, sentence):
         doc = self.nlp(sentence)
         return [ent.__str__() for ent in doc.ents]
+    def get_vec(self, sentence):
+        doc = self.nlp(sentence)
+        return doc.vector
