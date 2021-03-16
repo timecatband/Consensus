b3f1db80bf5574103f2ce406cc67523565fdea90
Kris Winquist <mentheosis@gmail.com>
2021-03-16 00:46:33 -0700

    remove console log

--- journal_app/client/public/services/GraphData.ts
+++ journal_app/client/public/services/GraphData.ts
@@ -89,7 +89,6 @@ class GraphData { // this thing should probably just extend EventEmitter
   // The initial graph load response
   handleServerGraphResponse(graphData: GraphModel) {
     const newGraph = GraphModel.deSerialize(graphData)
-    console.log("test", GraphModel, newGraph)
     this.graphs.push(newGraph)
     this.setDisplayedGraph(newGraph)
   }
