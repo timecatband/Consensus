bfc87013807e9cf33a678f634657ab13d1c2e2d8
Dan <dmedani@yelp.com>
2021-03-15 22:53:52 -0700

    some panel textuals

--- journal_app/client/public/components/side_panel/SingleNodePanel.tsx
+++ journal_app/client/public/components/side_panel/SingleNodePanel.tsx
@@ -7,15 +7,24 @@ function SingleNodePanel(props: any): any {
   return (
     <div>
       <div className='panelHeader'>
+        <div className="panelText">
+          Label:
+        </div>
         <input type="text"
           onChange={event => props.updateItem({label:event.target.value})}
           value={props.itemLabel}>
         </input>
       </div>
+      <div className="panelText">
+        Link:
+      </div>
       <input className="itemLink"
         onChange={event => props.updateItem({link:event.target.value})}
         value={props.itemLink}>
       </input>
+      <div className="panelText">
+        Text:
+      </div>
       <textarea className="itemText"
           onChange={event => props.updateItem({text:event.target.value})}
           value={props.itemText}
