8d5167b8f53fbe11403945971ab77bdbb7a118a4
Dan <dmedani@yelp.com>
2021-03-17 01:43:22 -0700

    fix contract pragmas

--- public_square/contracts/Consensus.sol
+++ public_square/contracts/Consensus.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.5.16 <0.9.0;
 
 contract ConsensusGraph {
   struct Node {
--- public_square/contracts/Migrations.sol
+++ public_square/contracts/Migrations.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.5.16 <0.9.0;
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  constructor() {
+  constructor () public {
     owner = msg.sender;
   }
 
